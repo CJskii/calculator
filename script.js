2d05bfc4131f057b080ec36212886ae9eb3a4704
CJskii <czareczarek3@gmail.com>
2022-08-05 23:46:24 +0100

    created document variables and button event listener

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const hi = "Hello World!"
+ const container = document.querySelector('.container')
+ const display = document.querySelector('.display')
+ const buttons = document.querySelectorAll('.btn')
+ const calculation = document.querySelector('.calculation')
+ const userInput = document.querySelector('.user-input')
+
+ buttons.forEach((button => button.addEventListener('click', function () {
+    userInput.textContent += this.textContent
+ })))
